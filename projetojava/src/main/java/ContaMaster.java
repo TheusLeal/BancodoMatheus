72f197e33244268d48a9fae60d330953571367bf
Matheus <mthm37@gmail.com>
2017-08-24 16:35:16 -0300

    bhu

---
+++ projetojava/src/main/java/ContaMaster.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class ContaMaster {
+
+    /**
+     * @return the saldo_investimento
+     */
+    public float getSaldo_investimento() {
+        return saldo_investimento;
+    }
+
+    /**
+     * @param saldo_investimento the saldo_investimento to set
+     */
+    public void setSaldo_investimento(float saldo_investimento) {
+        this.saldo_investimento = saldo_investimento;
+    }
+
+    /**
+     * @return the limete_extra
+     */
+    public float getLimete_extra() {
+        return limete_extra;
+    }
+
+    /**
+     * @param limete_extra the limete_extra to set
+     */
+    public void setLimete_extra(float limete_extra) {
+        this.limete_extra = limete_extra;
+    }
+
+    private float saldo_investimento;
+private float limete_extra;
+
+
+
+
+    
+}
